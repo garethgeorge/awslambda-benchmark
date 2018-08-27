ef89cbfeb12be1fa2b3acc30e26825230dbc1af8
Gareth George <garethgeorge97@gmail.com>
2018-08-27 15:24:55 -0700

    updated with parallel testing capabilities

--- benchmark-series.py
+++ benchmark-series.py
@@ -1,23 +1,38 @@
 import argparse 
 import json 
-import boto3
 import sys, os
+import queue
+import threading
+
+import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
 
+
 parser = argparse.ArgumentParser(description='Run a benchmark')
 parser.add_argument('mode', help='are we using the AWS endpoint or localhost?')
+parser.add_argument('--parallelism', default=1, type=int, 
+    help='how many threads to test on, run completes when last thread terminates')
+parser.add_argument('--count', default=100, type=int, 
+    help='how many threads to test on, run completes when last thread terminates')
 args = parser.parse_args()
 
 if args.mode.lower() == "localhost": 
     print("LOCALHOST MODE")
     awslambda = boto3.client('lambda', 
-        config=Config(signature_version=UNSIGNED),
+        config=Config(signature_version=UNSIGNED, region='us-west-2'),
         endpoint_url="http://localhost:80"
     )
 
-elif args.mode.lower() == "aws":
+elif args.mode.lower() == "ec2":
+    
+    print("Using a hard-coded ec2 instance")
+    awslambda = boto3.client('lambda', 
+        config=Config(signature_version=UNSIGNED),
+        endpoint_url="http://cspot.lastpengu.in:80"
+    )
 
+elif args.mode.lower() == "aws":
     print("AWS MODE")
     with open('aws.json', 'r') as f:
         aws_creds = json.load(f)
@@ -26,24 +41,45 @@ elif args.mode.lower() == "aws":
         aws_access_key_id=aws_creds["aws_access_key_id"],
         aws_secret_access_key=aws_creds["aws_secret_access_key"],
     )
-
 else:
 
     print("Invalid mode was specified.")
     sys.exit(1)
 
 
-payload = b"""{
-  "a": 15,
-  "b": 22
-}"""
 
-for x in range(0, 100):
-    print("trying to call...")
-    response = awslambda.invoke(
-        FunctionName="add",
-        InvocationType='RequestResponse',
-        Payload=payload
-    )  
-    print(response)
-    print(response["Payload"].read())
+jobqueue = queue.Queue()
+resultqueue = queue.Queue()
+
+for i in range(args.count):
+    
+    def job(thread_id):
+        print("lambda invoke on thread: %d" % thread_id)
+        payload = b"""{
+            "a": 15,
+            "b": 22
+        }"""
+        response = awslambda.invoke(
+            FunctionName="add",
+            InvocationType='RequestResponse',
+            Payload=payload
+        )
+        print("thread %d response: %s" % (thread_id, str(response)))
+        return response["Payload"].read()
+    
+    jobqueue.put(job)
+
+def run_test(jobqueue, resultqueue, thread_id):
+    while True:
+        job = jobqueue.get()
+        resultqueue.put(job(thread_id))
+
+for i in range(args.parallelism):
+    print("spawning thread #%d" % i)
+    t = threading.Thread(target=run_test, args = (jobqueue, resultqueue, i))
+    t.daemon = True
+    t.start()
+
+for x in range(args.count):
+    print("got result #%d: %s" % (x, resultqueue.get().decode('ascii')))
+    
